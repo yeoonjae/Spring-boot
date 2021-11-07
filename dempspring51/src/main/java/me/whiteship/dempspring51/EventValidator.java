fbbde83cc66041e15d4ed3e07c122b0266c8f50e
duswo5310 <63777714+duswo5310@users.noreply.github.com>
2021-11-07 15:08:02 +0900

    스프링 핵심기술 실습코드

---
+++ dempspring51/src/main/java/me/whiteship/dempspring51/EventValidator.java
@@ -0,0 +1,18 @@
+package me.whiteship.dempspring51;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+public class EventValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Event.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "title","notEmpty","Empty title is not allowed.");
+    }
+}

d5515a2a0e2823385f67124a6d575b7a15b6eda5
duswo5310 <63777714+duswo5310@users.noreply.github.com>
2021-12-05 16:03:08 +0900

    [인프런]HTTP 요청 맵핑하기 연습문제

--- demo-web-mvc/src/main/java/com/infren/study/demowebmvc/SampleController.java
+++ demo-web-mvc/src/main/java/com/infren/study/demowebmvc/SampleController.java
@@ -3,24 +3,42 @@ package com.infren.study.demowebmvc;
 import java.awt.PageAttributes.MediaType;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.function.ServerRequest.Headers;
 import org.w3c.dom.html.HTMLTableCaptionElement;
 
 @Controller
+@RequestMapping("/events")
 public class SampleController {
 
     @ResponseBody
-    @GetHelloMapping
-    public String hello() {
-        return "hello";
+    @GetMapping("/{number}")
+    public String event(@PathVariable String number) {
+        return "events " + number;
     }
-//    @PostMapping("/hello")
-//    public String hello() {
-//        return "hello";
-//    }
+
+    @ResponseBody
+    @PostMapping(value = "/",consumes = "application/json", produces = "application/json")
+    public String eventPost() {
+        return "events ";
+    }
+
+    @ResponseBody
+    @DeleteMapping("/{number}")
+    public String eventDelete(@PathVariable String number) {
+        return "events "+ number;
+    }
+
+    @ResponseBody
+    @PutMapping(value = "/{number}", produces = "application/json",consumes = "application/json")
+    public String eventPut(@PathVariable String number) {
+        return "events "+ number;
+    }
 
 }
--- demo-web-mvc/src/test/java/com/infren/study/demowebmvc/SampleControllerTest.java
+++ demo-web-mvc/src/test/java/com/infren/study/demowebmvc/SampleControllerTest.java
@@ -1,6 +1,7 @@
 package com.infren.study.demowebmvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -15,6 +16,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,12 +27,48 @@ class SampleControllerTest {
     MockMvc mockMvc;
 
     @Test
-    public void helloTest() throws Exception{
-        mockMvc.perform(get("/hello"))
+    public void eventTest1() throws Exception{
+        mockMvc.perform(get("/events/1"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(content().string("events 1"));
+        mockMvc.perform(get("/events/2"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(content().string("events 2"));
+        mockMvc.perform(get("/events/3"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(content().string("events 3"));
+    }
+
+    @Test
+    public void eventTestPost() throws Exception{
+        mockMvc.perform(post("/events/")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
             .andDo(print())
             .andExpect(status().isOk());
-
-
     }
+
+    @Test
+    public void eventTestDelete() throws Exception{
+        mockMvc.perform(delete("/events/1"))
+            .andDo(print())
+            .andExpect(content().string("events 1"))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    public void eventTestPut() throws Exception{
+        mockMvc.perform(put("/events/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(content().string("events 1"))
+            .andExpect(status().isOk());
+    }
+
+
 
 }

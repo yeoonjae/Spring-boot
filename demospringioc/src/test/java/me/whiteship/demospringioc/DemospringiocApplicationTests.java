462637b2f11f01c4da063b6bea0e5fad44fc65c2
duswo5310 <63777714+duswo5310@users.noreply.github.com>
2022-01-12 03:42:51 +0900

    secrets Test

--- demospringioc/src/test/java/me/whiteship/demospringioc/DemospringiocApplicationTests.java
+++ demospringioc/src/test/java/me/whiteship/demospringioc/DemospringiocApplicationTests.java
@@ -3,8 +3,10 @@ package me.whiteship.demospringioc;
 import me.whiteship.demospringioc.book.BookService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DemospringiocApplicationTests {
 
 	@Test
